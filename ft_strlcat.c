110fe33357d731fec6872cf21e52c7265d5ee486
Arpy500 <renatoelp@outlook.com>
2022-11-22 01:00:46 +0000

    Added ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: relouren <relouren@student.42.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 13:46:11 by relouren          #+#    #+#             */
+/*   Updated: 2022/11/21 13:46:11 by relouren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (dst[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		if (size > i + 1)
+			dst[i + j] = src[j];
+		else if (size == i + 1)
+			dst[i + j] = '\0';
+		j++;
+		size--;
+	}
+	return (i + j);
+}
--- libft.h
+++ libft.h
@@ -37,4 +37,6 @@ void			*ft_memmove(void *dest, const void *src, size_t n);
 
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+
 #endif
